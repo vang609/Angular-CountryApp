7638599bc48b396c25526af015cdb356eaa3f7c8
en-vang <enrique@si.team>
2023-11-08 21:11:24 -0500

    Code refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { SharedModule } from './shared/shared.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule, AppRoutingModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-country-page',
+  templateUrl: './by-country-page.component.html',
+  styles: [],
+})
+export class ByCountryPageComponent implements OnInit {
+  public countries: Country[] = [];
+  public initialValue: string = '';
+
+  constructor(private countriesService: CountriesService) {}
+  ngOnInit(): void {
+    this.countries = this.countriesService.cacheStore.byCountries.countries;
+    this.initialValue = this.countriesService.cacheStore.byCountries.term;
+  }
+  searchByCountry(term: string) {
+    this.countriesService.searchCountry(term).subscribe((countries) => {
+      this.countries = countries;
+    });
+  }
+}
